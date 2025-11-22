03f7a55ccb81fd528145e43991e88466742d701c
shou xiang <weeshouxiang@gmail.com>
2025-11-22 14:25:56 +0800

    added ast code

--- headers/ast.h
+++ headers/ast.h
@@ -6,13 +6,14 @@
 /*   By: wshou-xi <wshou-xi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 14:26:34 by wshou-xi          #+#    #+#             */
-/*   Updated: 2025/11/14 14:31:15 by wshou-xi         ###   ########.fr       */
+/*   Updated: 2025/11/21 11:32:49 by wshou-xi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef AST_H
 # define AST_H
 
+#include <linux/limits.h>
 typedef struct s_ast
 {
 	char			*token;
@@ -21,7 +22,10 @@ typedef struct s_ast
 	struct s_ast	*prev;
 }	t_ast;
 
-
-
+typedef enum e_node_type
+{
+	PIPE,
+	SIMPLE_COM
+}	t_node_type;
 
 #endif
