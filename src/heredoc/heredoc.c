441aa3cc001c0b28c15326a5320ba1782c7a77b9
Joven <194885329+joven-l@users.noreply.github.com>
2025-11-24 12:14:01 +0800

    files for project structure

---
+++ src/heredoc/heredoc.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 10:26:04 by joloo             #+#    #+#             */
+/*   Updated: 2025/11/24 11:39:31 by joloo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// << should be given a delimiter
+// then read the input until a line containing the delimiter is seen.
+// However, it doesn’t have to update the history!
+
+// returns fdin of a pipe
+// returns -1 on error
+int	heredoc(char *delimiter)
+{
+
+}
